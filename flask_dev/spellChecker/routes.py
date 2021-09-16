af22046e002fa8042544c00d4e53d32cb1defe1b
mfdunlap <maya.dunlap@slu.edu>
2021-09-16 10:16:56 -0500

    Adding html pages for supported languages
    Adds html pages for Spanish, French, German, and Portuguese. Makes clicking on the website icon take user to default page and adds functionality to navbar.

--- flask_dev/spellChecker/routes.py
+++ flask_dev/spellChecker/routes.py
@@ -4,6 +4,22 @@ from flask import render_template
 
 @app.route('/')
 @app.route('/home')
-@app.route('/eng')
+@app.route('/english')
 def default_page():
     return render_template('english.html')
+
+@app.route('/french')
+def french_page():
+    return render_template('french.html')
+
+@app.route('/german')
+def german_page():
+    return render_template('german.html')
+
+@app.route('/portuguese')
+def portuguese_page():
+    return render_template('portuguese.html')
+
+@app.route('/spanish')
+def spanish_page():
+    return render_template('spanish.html')
