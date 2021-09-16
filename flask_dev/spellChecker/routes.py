63ed64d35547009b54b34d0b1fdff3610f04b1ce
lorenzopappone <lorenzo.pappone@slu.edu>
2021-09-16 17:09:11 -0500

    sample

--- flask_dev/spellChecker/routes.py
+++ flask_dev/spellChecker/routes.py
@@ -2,10 +2,14 @@
 import requests
 from spellChecker import utils, app
 from flask import render_template, request
+from flask import Flask, jsonify, request, redirect, render_template
+import os, json
 
+"""
 @app.route('/')
 @app.route('/home')
 @app.route('/eng')
+
 def default_page():
     return render_template('english.html')
 
@@ -23,4 +27,56 @@ def submit():
         #print(raw)
     #return 'You entered: {}'.format(request.form['message'])
     return render_template('english.html', errors=errors, results=results)
+"""
+"""
+@app.route("/", methods=["POST", "GET"])
+def home():
+	if request.method == "GET":
+		languages = ["C++", "Python", "PHP", "Java", "C", "Ruby",
+					"R", "C#", "Dart", "Fortran", "Pascal", "Javascript"]
+		
+		return render_template("index.html", languages=languages)
+"""
 
+@app.route('/')
+def upload_form():
+	return render_template('suggestions.html')
+
+
+@app.route('/search', methods=['POST'])
+def search():
+
+        # Retrieve test
+        term = request.form['q']
+        print('term: ', term)
+        # Get the mispelled and the candidates
+        candidates, misspelled = utils.simpleChecker(term)
+        # Get the last mispelled word candidates
+        if misspelled:
+            last_mispelled = misspelled[len(misspelled)-1]
+            print("last mispelled : ", last_mispelled)
+            last_candidate = candidates[str(last_mispelled)]
+            #print("last candidate : ", candidates[str(last_mispelled)])
+            last_candidate = list([c for c in last_candidate])
+            print('last candidate : ', last_candidate)
+
+
+        #print("all mispelled : ", misspelled)
+        #print("all candidates : ", candidates)
+        
+        #corrected_text = term.replace(last_mispelled)
+        #json_cand = jsonify(candidates)
+        #print(json_cand)
+        # Set json url for results
+        SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+        json_url = os.path.join(SITE_ROOT, "data", "results.json")
+        json_data = json.loads(open(json_url).read())
+        #print (json_data)
+        #print (json_data[0])
+        # Compute
+        filtered_dict = [v for v in json_data if term in v]
+        #print(filtered_dict)
+        resp = jsonify(last_candidate if misspelled else None)
+        print(resp)
+        resp.status_code = 200
+        return resp
--- flask_dev/spellChecker/utils.py
+++ flask_dev/spellChecker/utils.py
@@ -6,25 +6,26 @@ import re
 def simpleChecker(text):
     
     # Read file
-    print("Original Text:\n", str(text))
+    #print("Original Text:\n", str(text))
     # Remove punctuation using regex
     s = re.sub(r'[^\w\s]','', text)
-    print("Text without punctuations:\n",s)
+    #print("Text without punctuations:\n",s)
     wordlist=s.split()
     spell = SpellChecker()
     misspelled = list(spell.unknown(wordlist))
-    print("Possible list of misspelled words in the original text:\n",misspelled)
+    #print("Possible list of misspelled words in the original text:\n",misspelled)
 
     # Use pyspellchecker to correct the word and list candidates
     candidates = dict()
     for word in misspelled:
         # Get the one `most likely` answer
-        print("Correct word:",spell.correction(word))
+        #print("Correct word:",spell.correction(word))
         # Get a list of `likely` options
-        print("Candidate words:",spell.candidates(word))
+        #print("Candidate words:",spell.candidates(word))
         candidates[word] = spell.candidates(word)
     
-    print(candidates)
+    #print(candidates)
+    return candidates, misspelled
         
 
      
