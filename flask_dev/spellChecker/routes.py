04dae938c7364fe95624a967519d5f4aa93d37b7
lorenzopappone <lorenzo.pappone@slu.edu>
2021-09-28 15:32:19 -0500

    add word selection

--- flask_dev/spellChecker/routes.py
+++ flask_dev/spellChecker/routes.py
@@ -39,13 +39,27 @@ def home():
 """
 
 @app.route('/')
-def upload_form():
-	return render_template('suggestions.html')
+def default_page():
+	return render_template('english.html')
 
+@app.route('/submit', methods=['POST'])
+def submit():
+    results = {}
+    errors = []
+    #if request.method=='POST':
+    #    try:
+    #        text = request.form['message']
+            #print(text)
+            #utils.simpleChecker(text)
+    #    except:
+    #        errors.append("Unable to get text.")
+        #print(raw)
+    #return 'You entered: {}'.format(request.form['message'])
+    return render_template('english.html', errors=errors, results=results)
 
 @app.route('/search', methods=['POST'])
 def search():
-
+    
         # Retrieve test
         term = request.form['q']
         print('term: ', term)
