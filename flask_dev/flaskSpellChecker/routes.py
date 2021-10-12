ddc0da31c41e45b4896104076a6a345f1244cc45
mfdunlap <maya.dunlap@slu.edu>
2021-10-12 12:07:26 -0500

    Additional Commenting
    Details how the added language changes can be seen and tested.

--- flask_dev/flaskSpellChecker/routes.py
+++ flask_dev/flaskSpellChecker/routes.py
@@ -8,30 +8,35 @@ import os, json
 @app.route('/english')
 def default_page():
     # Set language to UTF-8 code for English
+    # Can be tested by looking at viewing source code on page (Ctrl+U)
     language = "en"
     return render_template('english.html', language=language)
 
 @app.route('/french')
 def french_page():
     # Set language to UTF-8 code for French
+    # Can be tested by looking at viewing source code on page (Ctrl+U)
     language = "fr"
     return render_template('french.html', language=language)
 
 @app.route('/german')
 def german_page():
     # Set language to UTF-8 code for German
+    # Can be tested by looking at viewing source code on page (Ctrl+U)
     language = "de"
     return render_template('german.html', language=language)
 
 @app.route('/portuguese')
 def portuguese_page():
     # Set language to UTF-8 code for Portuguese
+    # Can be tested by looking at viewing source code on page (Ctrl+U)
     language = "pt"
     return render_template('portuguese.html', language=language)
 
 @app.route('/spanish')
 def spanish_page():
     # Set language to UTF-8 code for Spanish
+    # Can be tested by looking at viewing source code on page (Ctrl+U)
     language = "es"
     return render_template('spanish.html', language=language)
     
