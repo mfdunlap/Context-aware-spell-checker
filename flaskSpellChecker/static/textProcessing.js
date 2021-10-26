81b19dfac78f645542f484190948725370bae55f
mfdunlap <maya.dunlap@slu.edu>
2021-10-25 23:04:37 -0500

    Add right click function for replacement menu
    Add function to open suggestions menu on right click
    Restructure right click and double click function to minimize repeated code

--- flaskSpellChecker/static/textProcessing.js
+++ flaskSpellChecker/static/textProcessing.js
@@ -12,73 +12,113 @@ $(document).ready(function () {
   textArea.css({ cursor: 'pointer' });
 
   // Get word on double click
-  textArea.dblclick(function (e) {
-    var selection = window.getSelection() || document.getSelection() || document.selection.createRange();
-    var word = $.trim(selection.toString())// Ajax integration: selected word is forwarded to backend through POST request
+  textArea.dblclick(function (event) {
+    var selection = window.getSelection();
+    var word = $.trim(selection.toString());
 
-    // Get the list of suggested corrections for the selected word if it is misspelled
-    console.log(word)
-    $.ajax({
-      type: "POST",
-      url: "http://localhost:5000/selected",
-      cache: false,
-      dataType: "json",
-      data: {
-        'test': word,
-      },
-
-      // If word is misspelled
-      success: function (suggestions) {
-        console.log(suggestions);
-
-        if (word != '') {
-          var list_item_str = ''
-
-          // Add each suggestion to the dropdown list
-          for (var key in suggestions) {
-            var suggest = suggestions[key]
-            console.log(suggest)
-
-            list_item_str += '<li data-action="' + suggest + '">' + suggest + '</li>'
-          }
-
-          // Put the new list items into the html code for the dropdown menu
-          $('#listItems').empty().append(list_item_str);
-          console.log(document.getElementById("contextMenu").innerHTML);
-
-          // Show the dropdown menu
-          $(".context-menu").finish().toggle(100).
-
-            // Move it to where the mouse is clicked
-            css({
-              top: e.pageY + "px",
-              left: e.pageX + "px"
-            });
-
-          // If the document is clicked somewhere
-          $(document).bind("mousedown", function (e) {
-            // If the clicked element is not the menu
-            if (!$(e.target).parents(".context-menu").length > 0) {
-
-              // Hide the menu
-              $(".context-menu").hide(100);
-            }
-          });
-
-
-          // If a menu element is clicked
-          $(".context-menu li").click(function () {
-
-            // Replaced the misspelling with the selected correction
-            var correction = $(this).attr("data-action");
-            document.getElementById("textArea").innerHTML = document.getElementById("textArea").innerHTML.replace('<span style="text-decoration: underline 2px red;">'+word+'</span>', correction);
-
-
-            // Hide the menu after the action was triggered
-            $(".context-menu").hide(100);
-          });
-        }
-      },
-    });
+    console.log(word);
+    displayMisspellings(word, event);
   });
 });
+
+// Get word on right click
+$("#textArea").bind("contextmenu", function (event) {
+  // Avoid the default contextMenu
+  event.preventDefault();
+
+  var selection = window.getSelection();
+  var word = selectionToString(selection);
+
+  console.log(word);
+  displayMisspellings(word, event);
+});
+
+function displayMisspellings(wordToCheck, event) {
+  // Get the list of suggested corrections for the selected word if it is misspelled
+  // Display mispellings in a context menu
+
+  $.ajax({
+    type: "POST",
+    url: "http://localhost:5000/selected",
+    cache: false,
+    dataType: "json",
+    data: {
+      'test': wordToCheck,
+    },
+
+    // If word is misspelled
+    success: function (suggestions) {
+      docArea = "textArea"
+      misspelling = '<span style="text-decoration: underline 2px red;">' + wordToCheck + '</span>'
+      menu = ".context-menu"
+      menuItem = ".context-menu li"
+      menuItemID = '#listItems'
+      console.log(suggestions);
+
+      setMenuItems(menuItemID, suggestions);
+      console.log(document.getElementById("contextMenu").innerHTML);
+
+      showMenu(menu, event);
+
+      // If a menu item is clicked
+      $(menuItem).click(function () {
+
+        replaceMisspellingWithItem(docArea, misspelling, this);
+        hideMenu(menu);
+      });
+
+      // If the document is clicked somewhere else
+      $(document).bind("mousedown", function (event) {
+        if (!$(event.target).parents(menu).length > 0) {
+          hideMenu(menu);
+        }
+      });
+    },
+  });
+}
+
+function setMenuItems(menu, items) {
+  var menuItems = ''
+
+  for (var item in items) {
+    var itemVal = items[item];
+    //console.log(itemVal);
+    menuItems += '<li data-action="' + itemVal + '">' + itemVal + '</li>';
+  }
+
+  $(menu).empty().append(menuItems);
+}
+
+function showMenu(menu, event) {
+  $(menu).finish().toggle(100).
+    css({
+      top: event.pageY + "px",
+      left: event.pageX + "px"
+    });
+}
+
+function replaceMisspellingWithItem(docArea, misspelling, item) {
+  var correction = $(item).attr("data-action");
+  document.getElementById(docArea).innerHTML = document.getElementById(docArea).innerHTML.replace(misspelling, correction);
+
+}
+
+function hideMenu(menu) {
+  $(menu).hide(100);
+}
+
+function selectionToString(selection) {
+  var range = selection.getRangeAt(0);
+  var node = selection.anchorNode;
+
+  while (range.toString().indexOf(' ') != 0 && range.startOffset != 0) {
+    range.setStart(node, (range.startOffset - 1));
+  }
+  range.setStart(node, range.startOffset);
+
+  do {
+    range.setEnd(node, range.endOffset + 1);
+  } while (range.toString().indexOf(' ') == -1 && range.toString().trim() != '' && range.endOffset < range.endContainer.length);
+
+  return range.toString().trim();
+}
