37486751818bba982f9d5393a98a38b16b8d93f3
mfdunlap <maya.dunlap@slu.edu>
2021-11-28 19:09:41 -0600

    Edit dictionary testing
    Makes edits to files to allow for dictionary to work with the rest of the packages for the spell checker

--- flaskSpellChecker/__init__.py
+++ flaskSpellChecker/__init__.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'b00716a7c0e15501c1ba3ba0' 
 
 from flaskSpellChecker import routes
-from flaskSpellChecker import dictionary
-from dictionary import Dictionary
+#from flaskSpellChecker import dictionary
+#from dictionary import Dictionary
 
-ga = Dictionary('ga')
+#ga = Dictionary('ga')
 # en = Dictionary('en')
