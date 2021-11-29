bb29446b46418161bdc40876dc0de4fbac0178f4
mfdunlap <maya.dunlap@slu.edu>
2021-11-28 19:39:17 -0600

    Give DB Access
    Provide database access to spell checker

--- flaskSpellChecker/__init__.py
+++ flaskSpellChecker/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/websiteText.db'
 app.config['SECRET_KEY'] = 'b00716a7c0e15501c1ba3ba0' 
 
-from flaskSpellChecker import routes
-from flaskSpellChecker import dictionary
+db = SQLAlchemy(app)
+
+from flaskSpellChecker import routes, dictionary
 from dictionary import Dictionary
 
 ga = Dictionary('ga')
