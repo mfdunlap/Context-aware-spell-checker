454b63d873bd477b3ab5c7e74fb76e2435eb29a1
lorenzopappone <lorenzo.pappone@slu.edu>
2021-12-07 20:18:58 -0600

    add separate js file for the text class

--- flaskSpellChecker/__init__.py
+++ flaskSpellChecker/__init__.py
@@ -5,6 +5,3 @@ app.config['SECRET_KEY'] = 'b00716a7c0e15501c1ba3ba0'
 
 from flaskSpellChecker import routes
 from flaskSpellChecker._dictionary import Dictionary
-
-#ga = Dictionary('ga')
-#en = Dictionary('en')
