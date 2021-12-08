118960caf1c96ed115e4e4f93d2df9f5b87f7e51
lorenzopappone <lorenzo.pappone@slu.edu>
2021-12-07 20:21:06 -0600

    implemented support for duplicates and add integration with text class functions

--- flaskSpellChecker/static/textProcessing.js
+++ flaskSpellChecker/static/textProcessing.js
@@ -10,14 +10,17 @@ $(document).ready(function () {
   // Set cursor to hand to show that area is clickable
   var textArea = $("#textArea");
   textArea.css({ cursor: 'pointer' });
-
+  var textAreaElement = document.getElementById("textArea")
   // Get word on double click
   textArea.dblclick(function (event) {
-    var selection = window.getSelection();
+    var selection = document.getSelection();
     var word = $.trim(selection.toString());
 
-    console.log(word);
-    displayMisspellings(word, event);
+    selectedWordIndex = getSelectedWordIndex(textAreaElement, word)
+    //console.log("index: ",selectedWordIndex)
+
+    console.log("selected:",word);
+    displayMisspellings(word, event, selectedWordIndex);
   });
 });
 
@@ -28,12 +31,63 @@ $("#textArea").bind("contextmenu", function (event) {
 
   var selection = window.getSelection();
   var word = selectionToString(selection);
-
   console.log(word);
   displayMisspellings(word, event);
 });
 
-function displayMisspellings(wordToCheck, event) {
+function getSelectedWordIndex(textAreaElement, word, divId = "#textArea"){
+  // This function get the index of the selected word within the text area. It support duplicate misspelled word
+
+  // Get the offset of the last selected words' character 
+  selectionOffset = getCaretCharacterOffsetWithin(textAreaElement)
+  wordLength = word.length
+  // Get the portion of the text from the start to the selected word
+  substring = $(divId).text().substring(0, selectionOffset)
+  subStringWords = substring.split(/\s/)
+  let indices = []
+  // Get the indices of all the potential duplicates of the words
+  subStringWords.forEach((el,index) => {
+    if (el==word)
+      indices.push(index)
+  })
+  // If any, get the context of the word with the larger index (the actual selected)
+  var selectedWordIndex;
+  if (indices.length>1)
+    selectedWordIndex = indices[indices.length-1];
+  else
+    selectedWordIndex = indices[0]
+
+  return selectedWordIndex
+}
+
+function getCaretCharacterOffsetWithin(element) {
+  // This utility get the offset of the last character of the selected word (the caret offset)
+
+  var caretOffset = 0;
+  var doc = element.ownerDocument || element.document;
+  var win = doc.defaultView || doc.parentWindow;
+  var sel;
+  if (typeof win.getSelection != "undefined") {
+      sel = win.getSelection();
+      if (sel.rangeCount > 0) {
+          var range = win.getSelection().getRangeAt(0);
+          var preCaretRange = range.cloneRange();
+          preCaretRange.selectNodeContents(element);
+          preCaretRange.setEnd(range.endContainer, range.endOffset);
+          caretOffset = preCaretRange.toString().length;
+      }
+  } else if ( (sel = doc.selection) && sel.type != "Control") {
+      var textRange = sel.createRange();
+      var preCaretTextRange = doc.body.createTextRange();
+      preCaretTextRange.moveToElementText(element);
+      preCaretTextRange.setEndPoint("EndToEnd", textRange);
+      caretOffset = preCaretTextRange.text.length;
+  }
+  return caretOffset;
+}
+
+
+function displayMisspellings(wordToCheck, event, selectedWordIndex) {
   // Get the list of suggested corrections for the selected word if it is misspelled
   // Display mispellings in a context menu
 
@@ -53,17 +107,20 @@ function displayMisspellings(wordToCheck, event) {
       menu = ".context-menu"
       menuItem = ".context-menu li"
       menuItemID = '#listItems'
-      console.log(suggestions);
+      console.log("suggestions:",suggestions);
 
       setMenuItems(menuItemID, suggestions);
-      console.log(document.getElementById("contextMenu").innerHTML);
+      //console.log("corrections: ", document.getElementById("contextMenu").innerHTML);
 
       showMenu(menu, event);
 
       // If a menu item is clicked
       $(menuItem).click(function () {
+        //replaceMisspellingWithItem(docArea, misspelling, this);
 
-        replaceMisspellingWithItem(docArea, misspelling, this);
+        // Replace the misspelled word with the clicked correction.
+        replaceMisspellingWithItem(selectedWordIndex, this);
+        
         hideMenu(menu);
       });
 
@@ -82,7 +139,6 @@ function setMenuItems(menu, items) {
 
   for (var item in items) {
     var itemVal = items[item];
-    //console.log(itemVal);
     menuItems += '<li data-action="' + itemVal + '">' + itemVal + '</li>';
   }
 
@@ -97,10 +153,14 @@ function showMenu(menu, event) {
     });
 }
 
-function replaceMisspellingWithItem(docArea, misspelling, item) {
+function replaceMisspellingWithItem(misspellingIndex, item) {
   var correction = $(item).attr("data-action");
-  document.getElementById(docArea).innerHTML = document.getElementById(docArea).innerHTML.replace(misspelling, correction);
-
+  //document.getElementById(docArea).innerHTML = document.getElementById(docArea).innerHTML.replace(misspelling,  correction);
+  
+  // Change the word within the global text istance and its html code
+  textSingleton.replaceMisspelledWithCorrection(correction, misspellingIndex)
+  // Replace the old html text with the new one (with the correction)
+  textSingleton.replaceHtmlText()
 }
 
 function hideMenu(menu) {
